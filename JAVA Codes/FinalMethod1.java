31f08c743d083a0d8dad79e0fe0f9579b2ed5b2f
Tanishq chandole <tanishqchandole@gmail.com>
2026-02-14 23:24:52 +0530

    Codes

---
+++ JAVA Codes/FinalMethod1.java
@@ -0,0 +1,31 @@
+
+class Base
+{
+    public void fun() //Definition
+    {}
+    public void gun() //Definition
+    {}
+   
+}
+
+class Derived extends Base 
+{
+    public void run() //Definition
+    {}
+    public void gun() //overriding
+    {}
+}
+
+
+
+
+class FinalMethod1
+{
+    public static void main(String A[])
+    {
+        Demo dobj = new Demo();
+        
+
+       
+    }
+}
