04d64d11bc87c38029cf0d6f4d64761d0f4b2072
Tanishq chandole <tanishqchandole@gmail.com>
2026-02-23 23:03:29 +0530

    Codes

---
+++ JAVA Codes/ThreadPriority.java
@@ -0,0 +1,18 @@
+class ThreadPriority
+{ 
+    public static void main(String A[])
+    {
+        System.out.println("Inside main method");
+
+        Thread tobj = Thread.currentThread();
+        String name = tobj.getName();
+
+        System.out.println("Name of current is :"+name);
+        int Priority = tobj.getPriority();
+        System.out.println("Priority of current thread is:"+tobj.getPriority());
+
+        tobj.setPriority(10);
+        Priority = tobj.getPriority();
+        System.out.println("Priority of current thread is:"+tobj.getPriority());
+    }
+}
