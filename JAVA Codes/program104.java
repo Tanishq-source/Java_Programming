05fbfd6a7706f51d5cd54235617c3d98d3753d51
Tanishq chandole <tanishqchandole@gmail.com>
2026-04-03 23:10:42 +0530

    Codes

---
+++ JAVA Codes/program104.java
@@ -0,0 +1,51 @@
+// OOP Design 
+import java.util.Scanner;
+
+class ArrayX
+{
+    public int Arr[];
+    public int iSize;
+
+    public ArrayX(int no)
+    {
+        System.out.println("Inside Constructor");
+        iSize = no;
+        Arr = new int[iSize];
+    }
+
+    public void Accept()
+    {
+        int i = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter  the elements of array");
+
+        for(i = 0;i < Arr.length;i++)
+        {
+            Arr[i] = sobj.nextInt();
+        }
+    }
+
+    public void Display()
+    {
+        System.out.println("Elements of array are :");
+        int i = 0;
+
+        for(i = 0; i < Arr.length;i++)
+        {
+            System.out.println(Arr[i]);
+        }
+    }
+}
+
+class program105
+{
+    public static void main(String Arr[])
+    {
+        ArrayX aobj1 = new ArrayX(5);
+        ArrayX aobj2 = new ArrayX(7);
+
+       
+
+    }
+}
