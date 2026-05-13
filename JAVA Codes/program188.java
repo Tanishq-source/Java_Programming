a7a4eb05dcc12d01c96c4b331c0987ab64a4d216
Tanishq chandole <tanishqchandole@gmail.com>
2026-05-13 23:50:13 +0530

    Codes

---
+++ JAVA Codes/program188.java
@@ -0,0 +1,66 @@
+/*
+    iRow : 4
+    iCol : 4
+
+    Output :  $  $  $  $  
+              $  $  *  $
+              $  *  $  $
+              $  $  $  $
+                
+    Diagonal Pattern   
+    
+*/
+
+import java.util.Scanner;
+
+class Pattern
+{
+    // Filter for Square Matrix
+    public void Display(int iRow,int iCol)
+    {
+       int i = 0,j = 0;
+
+       if(iRow != iCol)                                                         // Need to write the filter in case of Diagonal Pattern
+       {
+         System.out.println("Invalid Input");
+         System.out.println("Row number and column number should be same");
+         return;
+       }
+       
+       for(i = 1;i <= iRow;i++)
+       {
+            for(j = 1;j <= iCol;j++)
+            {
+                if(i == j || i == 1 || i == iRow || j == 1 || j == iCol)
+                {
+                    System.out.print("$\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }      
+            }
+            System.out.println();
+       }
+    }
+}
+
+class program188
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0,iValue2 = 0;
+
+        System.out.println("Enter the Number of Rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the Number of Columns :");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2);
+    }
+}
+
